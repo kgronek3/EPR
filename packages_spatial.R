a49f3ad43e2c51a9da3756144dc29c794cd30adf
Krystian Gronek <k.gronek3@student.uw.edu.pl>
2022-10-15 20:59:03 +0200

    added packages for spatial_02.R

--- packages_spatial.R
+++ packages_spatial.R
@@ -4,9 +4,10 @@ packages_spatial <- c("spData", "terra", "tmap", "leafem", "spdep", "rgdal",
                       "maptools", "sp", "RColorBrewer", "classInt", "maps",
                       "prettymapr", "raster", "wesanderson", "quickPlot",
                       "viridis", "scales", "ggplot2", "shape", "sf",
-                      "cartography", "mapview", "ggrepel", "hexbin")
+                      "cartography", "mapview", "ggrepel", "hexbin",
+                      "dplyr")
 
-###############################################################################
+##############################################################################
 
 for (i in packages_spatial) {
   if (!(i %in% installed.packages()[, 1])) {
